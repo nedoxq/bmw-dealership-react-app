18711bd6dfbf1758afd2c7d7094cd5b71db0a62a
NedimHodzic04 <nedim.hodzic@stu.ibu.edu.ba>
2024-06-08 15:51:24 +0200

    Updates

---
+++ precache-manifest.f72703d223e9f3fcc3dbef815060c98e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7990b88f6cb3ded984c27b8b4583ad7",
+    "url": "/bmw-dealership-react-app/index.html"
+  },
+  {
+    "revision": "e27cd8202ca03b09ce08",
+    "url": "/bmw-dealership-react-app/static/css/main.825e723f.chunk.css"
+  },
+  {
+    "revision": "bbf27855d29e8ae76ebf",
+    "url": "/bmw-dealership-react-app/static/js/2.04d6b3f4.chunk.js"
+  },
+  {
+    "revision": "e27cd8202ca03b09ce08",
+    "url": "/bmw-dealership-react-app/static/js/main.f2a188d6.chunk.js"
+  },
+  {
+    "revision": "37a88a864ce9352cd61a",
+    "url": "/bmw-dealership-react-app/static/js/runtime~main.83f9f705.js"
+  },
+  {
+    "revision": "96387f856d6fa89ce0369ff200928b71",
+    "url": "/bmw-dealership-react-app/static/media/bmw-logo.96387f85.svg"
+  },
+  {
+    "revision": "1395165ebf74ac13de1708764af9c583",
+    "url": "/bmw-dealership-react-app/static/media/g30-m5-cs.1395165e.png"
+  },
+  {
+    "revision": "deccac4b6b947402e2c7d931d4b5da9e",
+    "url": "/bmw-dealership-react-app/static/media/g80-m3-sideshot.deccac4b.png"
+  },
+  {
+    "revision": "5df020568f7aa0795a8da46730493bd9",
+    "url": "/bmw-dealership-react-app/static/media/g80-m3.5df02056.png"
+  },
+  {
+    "revision": "0d0c25421d4b7c28370b11578814d87e",
+    "url": "/bmw-dealership-react-app/static/media/g82-m4.0d0c2542.png"
+  },
+  {
+    "revision": "f400d4f3c095fc7b6927ee86ee75d7d0",
+    "url": "/bmw-dealership-react-app/static/media/g87-m2.f400d4f3.png"
+  },
+  {
+    "revision": "08ded06843f5da839453b4b172d586e7",
+    "url": "/bmw-dealership-react-app/static/media/m8-comp.08ded068.png"
+  },
+  {
+    "revision": "4fc6533476562fe0df982e1f41cbaded",
+    "url": "/bmw-dealership-react-app/static/media/x5m.4fc65334.png"
+  }
+]);
