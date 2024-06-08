560458f9c2c465a0372caacfac58837dd664c450
NedimHodzic04 <nedim.hodzic@stu.ibu.edu.ba>
2024-06-08 16:21:47 +0200

    Fixes

--- src/components/HomePageTaglineComponent.js
+++ src/components/HomePageTaglineComponent.js
@@ -3,7 +3,7 @@ import { Typography } from "@mui/material";
 
 export default function HomePageTaglineComp(props){
     return(
-        <Typography variant="h1" align="justify" sx={{mt: 7.5, fontSize:{md:'64px', xs:'36px'}, fontWeight:700}}>
+        <Typography variant="h1" align="center" sx={{mt: 7.5, fontSize:{md:'64px', xs:'36px'}, fontWeight:700}}>
                 {props.tagline}
             </Typography>
     );
